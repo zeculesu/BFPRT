eeac9cf0204513a56a7516ba2dc597a778388e70
a.samoilova <artiyjana@yandex.ru>
2025-04-12 18:02:45 +0300

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zeculesu/Desktop/Artemia/algo_dop/dop2
-# Build directory: /home/zeculesu/Desktop/Artemia/algo_dop/dop2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
-subdirs("benchmarks")
--- build/tests/CMakeFiles/test_bfprt_impl.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_bfprt_impl.
-# This may be replaced when dependencies are built.
